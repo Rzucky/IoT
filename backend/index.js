3c8aebe642d0c5b6596617522d157d768469274e
Roko Mihalić <roko.mihalic@leapbit.com>
2023-05-23 03:13:54 +0200

    Website frontend, backend

---
+++ backend/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const md5 = require('md5');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+const usersJson = require('./userData.json');
+const mqtt = require('mqtt')
+
+
+const client  = mqtt.connect(`mqtt://${process.env.THINGHOST}`, {
+    username: process.env.TOKEN
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  for (const user of usersJson.users)
+  {
+    if (username === user.username && md5(password) === user.password)
+    {
+      // removing data values, not needed at frontend
+      const devs = []
+      for (const d of user.devices)
+      {
+        devs.push(
+          {name: d.name}
+        )
+      }
+      res.status(200).send({name: username, devices: devs});
+      return;
+    }
+    
+  }
+  res.status(200).json({ user: null });
+})
+
+
+app.post('/publish', async (req, res) => {
+  const { user, name } = req.body
+  let val = ''
+  for (const u of usersJson.users)
+  {
+    if (u.username === user.name)
+    {
+      for (const dev of u.devices)
+      { 
+        if (dev.name === name)
+        {
+          val = dev.data
+        }
+      }
+    }
+  }
+  
+  try
+  {
+    await client.publish("v1/devices/me/telemetry", JSON.stringify({'code': val}));
+    console.log('successfully sent publish data', val)
+  } 
+  catch (error) 
+  {
+      res.status(500).json({ message: 'mqtt error' });  
+  }
+
+  return;
+})
+
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000');
+});
---
+++ frontend/iot/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div v-if="!user" class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-4">
+        <h3 class="text-center">Login</h3>
+        <form @submit.prevent="submitForm">
+          <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="username" v-model="username" required>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" v-model="password" required>
+          </div>
+          <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+    <div v-else class="container border border-3 rounded-3 p-5 bg-light">
+
+    <h2 class="text-center mb-5">Welcome home, {{ user.name }}</h2>
+    <div v-for="(device, index) in user.devices" :key="index" class="row justify-content-center">
+      <div class="col-4">
+        <button @click="publish(device.name)"  class="btn btn-primary btn-lg btn-block mb-3">
+          <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+              <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+            </svg>
+            <span class="mx-3" >{{ device.name }}</span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+      return {
+          user: null,
+          username: '',
+          password: '',
+          name: ''
+      };
+    },
+
+  methods: {
+    async publish(device_name)
+    {
+      const resp = await axios.post('http://localhost:3000/publish', 
+        {
+          user: this.user,
+          name: device_name
+        }
+      )
+      if (resp.status === 500)
+      {
+        console.log(resp)
+      }
+    },
+
+    async submitForm()
+    {
+      const resp = await axios.post('http://localhost:3000/login', 
+        {
+          username: this.username, 
+          password: this.password
+        }
+      )
+      this.user = resp.data
+    }
+  }
+}
+</script>
